f9a1018594a7b0a3854453610c9fd6afd8acf260
Steve Binkowski <steve@binkcms.com>
2018-12-14 00:18:50 -0500

    Add account page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 ;
 (function ($, document, window, undefined) {
     $(document).ready(function () {
+        // Set the background colors for any product containers.
         $('.product-item-container').each(function () {
             $(this).css('background-color', ZJQ.randomColor());
         });
+
+        // Handle account page navigation
+        $('#account-nav').on('click', 'li', function(){
+            var $this = $(this),
+                target = $this.data('target')
+            ;
+            $('#account-nav li').removeClass('active');
+            $this.addClass('active');
+            $('#account-page main section').hide(0);
+            $('section#' + target).show(0);
+        });
+        $('#account-nav li.active').trigger('click');
+
+        // Light up the flipboards
+        // **** LIMITED TO THE DATE FIELD FOR PERFORMANCE ****
+        // $('.flipboard').each(function () {
         $('.todays-date .flipboard').each(function () {
             var $this = $(this),
                 this_opts = $this.data('flip-option'),
