52e45231f77c8139afa3d952852515f388ba1fba
Steve Binkowski <steve@binkcms.com>
2019-02-03 17:31:32 -0500

    Clean up to remove parser warnings

--- js/main.js
+++ js/main.js
@@ -1,15 +1,59 @@
 ;
 (function ($, document, window, undefined) {
   $(document).ready(function () {
-    // Set the background colors for any product containers.
-    //$('.product-item-container').each(function () {
-      /* $(this).css('background-color', ZJQ.randomColor());*/
-    //  $(this).addClass('product-color-' + ZJQ.randomColor());
-    //});
+    let $body = $('body');
+
+    // All hooks delegated through body element.
+    $body
+        // Hook the click event for products
+        .on('click', '.product-item-container', function (e) {
+          let $e = $(e.target);
+          if (!$e.hasClass('product-item-container')) {
+            $e = $e.closest('.product-item-container');
+          }
+          ZJQ.showProductOverlay($e);
+        })
+
+        // Hook to close the product overlay
+        .on('click', '.product-overlay-close', function (e) {
+          $(e.target).closest('.product-overlay-container').remove();
+        })
+
+        // Hook to handle plus/minus clicks on "Add to cart"
+        .on('click', '.add-to-cart-icon', function (e) {
+          e.preventDefault();
+          e.stopImmediatePropagation();
+          e.stopPropagation();
+          let $e = $(e.target),
+              $target = $e.closest('form').find('.add-to-cart-quantity'),
+              val = parseInt($target.val());
+          if ($e.hasClass('minus-icon')) {
+            if (--val < 1) {
+              val = 1;
+            }
+          }
+          else if ($e.hasClass('plus-icon')) {
+            val++;
+          }
+          $target.val(val);
+        })
+
+        // Hook to show/hide the cart.
+        .on('click', '#order-now-link', function (e) {
+          if (!$body.hasClass('no-cart')) {
+            e.preventDefault();
+            $body.toggleClass('show-cart');
+          }
+        })
+    ;
+
+    // Prevent the cart from showing on the landing page.
+    $('body#landing-page').addClass('no-cart');
+
 
     // Handle account page navigation
     $('#account-nav').on('click', 'li', function () {
-      var $this = $(this),
+      let $this = $(this),
           target = $this.data('target')
       ;
       $('#account-nav li').removeClass('active');
@@ -22,7 +66,7 @@
     // Light up the flipboards
     $('tr:first-child .flipboard').each(function () {
     //$('tr .flipboard').each(function () {
-      var $this = $(this),
+      let $this = $(this),
           this_opts = $this.data('flip-option'),
           opts = JSON.parse(JSON.stringify(ZJQ.flipboard_default_options))
       ;
@@ -34,52 +78,9 @@
 
     // Populate products and categories if a menu block exists.
     if ($('.category-list-container').length) {
-      //ZJQ.getProductCatalog();
       ZJQ.api.execute('catalog');
     }
 
-    // Hook the click event for products
-    $('body').on('click', '.product-item-container', function(e){
-      let $e = $(e.target);
-      if (!$e.hasClass('product-item-container')) {
-        $e = $e.closest('.product-item-container');
-      }
-      ZJQ.showProductOverlay($e);
-    });
-
-    // Hook to close the product overlay
-    $('body').on('click', '.product-overlay-close', function(e) {
-      $(e.target).closest('.product-overlay-container').remove();
-    })
-
-    // Hook to handle plus/minus clicks on "Add to cart"
-    $('body').on('click', '.add-to-cart-icon', function(e) {
-      e.preventDefault();
-      e.stopImmediatePropagation();
-      e.stopPropagation();
-      let $e = $(e.target),
-          $target = $e.closest('form').find('.add-to-cart-quantity'),
-          val = parseInt($target.val());
-      if ($e.hasClass('minus-icon')) {
-        val--;
-        if (val < 1) { val = 1; }
-      }
-      else if ($e.hasClass('plus-icon')) {
-        val++;
-      }
-      $target.val(val);
-    });
-
-    // Prevent the cart from showing on the landing page.
-    $('body#landing-page').addClass('no-cart');
-
-    // Hook to show/hide the cart.
-    $('body').on('click', '#order-now-link', function(e) {
-      if (!$('body').hasClass('no-cart')) {
-        e.preventDefault();
-        $('body').toggleClass('show-cart');
-      }
-    })
   });
 })
 (jQuery, document, window);
